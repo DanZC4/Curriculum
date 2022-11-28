f623b92ea0d495648d7b36584712fa5077a6531d
Daniel Zambrano <dzambranocortes@yahoo.com.mx>
2022-11-28 16:16:54 -0600

    abilities finished

--- components/Abilities.vue
+++ components/Abilities.vue
@@ -26,13 +26,13 @@
       </div>
     </div>
     <!-- abilidades -->
-    <div class="w-full h-[18rem] sm:h-[28rem]">
+    <div class="w-full h-[22rem] sm:h-[28rem]">
       <!-- stars -->
 
       <div class="flex justify-center overflow-hidden">
         <div class="px-0 sm:px-24 md:px-36 lg:px-48 pt-24 ant:pt-36 max-w-12xl 1xl:min-w-[100rem]">
 
-          <h2 class="text-3xl ant:text-4xl -mt-10 sm:mt-0 text-black font-bold">S<span
+          <h2 class="text-6xl 1xl:text-9xl -mt-10 sm:mt-0 text-black font-bold">S<span
               class="text-yellow-600">k</span>i<span class="text-gray-500">l</span><span
               class="text-yellow-600">l</span>s</h2>
           <div class="hidden sm:flex sm:w-[28rem]">
